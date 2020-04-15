a53c17503cf1147dde5c89046e9cd15cbb5c1c47
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-04-15 16:51:49 -0700

    javascripter

---
+++ app.js
@@ -0,0 +1,24 @@
+var today = new Date()
+ 
+if (monday) {greeting = 'Salad Day';}
+else if (tuesday) {greeting = 'Sushi Day';}
+else if (wednesday) {greeting} = 'Taco Day';} 
+else if (thursday) {greeting = 'Pho Day';}
+else if (friday) {greeting = 'Kale Day';}
+else if (saturday) {greeting = 'Quinoa Day';}
+else if (sunday) {greeting = 'Water Day';}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
