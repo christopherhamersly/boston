4def9bfe9acc53036840c4757bd13b26c294233e
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-04-16 12:18:48 -0700

    added functions

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 'use strict'
+
+
 alert('welcome to boston');
 
-prompt('your favorite food');
 
-confirm('yes');
+function favoriteFood () {
+    prompt('your favorite food');
+}
 
-var today = new Date();
-var minuteNow = today.getMinutes();
-var joke;
+function yes() {
+    confirm('yes');
+}
 
-if (minuteNow > 50) {
+function knockKnockJoke () {
+    var today = new Date();
+    var minuteNow = today.getMinutes();
+    var joke;
+
+    if (minuteNow > 50) {
     joke = 'Knock. Knock. Who’s there? Nobel. Nobel who? Nobel…that’s why I knocked!!';
-} else if (minuteNow > 40) {
+    } else if (minuteNow > 40) {
     joke = 'Knock. Knock. Who’s there? Tank. Tank who? You’re welcome.';
-} 
-else if (minuteNow > 30) {
+    } 
+    else if (minuteNow > 30) {
     joke = 'Who’s there? Annie. Annie who? Annie thing you can do, I can do too!';
-}
-else if (minuteNow > 20) {
+    }
+    else if (minuteNow > 20) {
     joke = 'Knock Knock. Who’s there? Luke. Luke who? Luke through the peep hole and find out.';
-} 
-else if (minuteNow > 10) { 
+    } 
+    else if (minuteNow > 10) { 
     joke = 'Who’s there? Cow says. Cow says who? No, a cow says mooooo!';
-} 
-else if (minuteNow > 0) {
+    } 
+    else if (minuteNow > 0) {
     joke = 'Orange you glad I didnt say banana';
-}
-else {
+    }
+    else {
     joke = 'Welcome';
 }
-document.write('<h3>' + joke + '</h3>');
+    document.write('<h3>' + joke + '</h3>');
+}
 
+favoriteFood ();
+yes ();
+knockKnockJoke ();
 
 
